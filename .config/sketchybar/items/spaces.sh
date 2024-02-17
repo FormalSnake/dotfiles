2554cf23c5421750f811ebb8778294babd2ea949
superlopez-real <kakskemail@gmail.com>
2024-02-17 11:30:22 +0200

    enhancements

--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -2,18 +2,21 @@
 
 SPACE_SIDS=(1 2 3 4 5 6 7 8 9 10)
 
-for sid in "${SPACE_SIDS[@]}"
-do
-  sketchybar --add space space.$sid left                                 \
-             --set space.$sid space=$sid                                 \
-                              background.corner_radius=100  \
-                              background.height=16 \
-                              script="$PLUGIN_DIR/space.sh"
+for sid in "${SPACE_SIDS[@]}"; do
+	sketchybar --add space space.$sid left \
+		--set space.$sid space=$sid \
+		icon=$sid \
+		label.font="sketchybar-app-font:Regular:16.0" \
+		label.padding_right=20 \
+		label.y_offset=-1 \
+		script="$PLUGIN_DIR/space.sh"
 done
 
-sketchybar --add item space_separator left                             \
-           --set space_separator icon="[\\\]"                                \
-                                 icon.color=$ACCENT_COLOR \
-                                 label.drawing=off                     \
-                                 background.drawing=off                \
-           --subscribe space_separator space_windows_change
+sketchybar --add item space_separator left \
+	--set space_separator icon="􀆊" \
+	icon.color=$ACCENT_COLOR \
+	icon.padding_left=4 \
+	label.drawing=off \
+	background.drawing=off \
+	script="$PLUGIN_DIR/space_windows.sh" \
+	--subscribe space_separator space_windows_change
--- .config/sketchybar/plugins/space.sh
+++ .config/sketchybar/plugins/space.sh
@@ -7,17 +7,17 @@
 source "$CONFIG_DIR/colors.sh" # Loads all defined colors
 
 if [ $SELECTED = true ]; then
-  sketchybar --set $NAME background.drawing=on \
-                     blur_radius=30   \
-                         background.color=$ACCENT_COLOR \
-                         label.color=$BAR_COLOR \
-                         width=32  \
-                         icon.color=$BAR_COLOR
+	sketchybar --set $NAME background.drawing=on \
+		blur_radius=30 \
+		background.color=$ACCENT_COLOR \
+		label.color=$BAR_COLOR \
+		width=32 \
+		icon.color=$BAR_COLOR
 else
-  sketchybar --set $NAME background.drawing=on \
-                     blur_radius=30   \
-                         background.color=$ITEM_BG_COLOR \
-                         label.color=$ACCENT_COLOR \
-                         width=16 \
-                         icon.color=$ACCENT_COLOR
+	sketchybar --set $NAME background.drawing=on \
+		blur_radius=30 \
+		background.color=$ITEM_BG_COLOR \
+		label.color=$ACCENT_COLOR \
+		width=16 \
+		icon.color=$ACCENT_COLOR
 fi
