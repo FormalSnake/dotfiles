eb52e401a3d24dbc1d8b39b533c8af8509df829e
superlopez-real <kakskemail@gmail.com>
2024-02-25 16:01:06 +0000

    fixed some padding

--- .config/sketchybar/items/battery.sh
+++ .config/sketchybar/items/battery.sh
@@ -9,5 +9,7 @@ sketchybar --add item battery right \
 	--set battery update_freq=120 \
 	script="$PLUGIN_DIR/battery.sh" \
 	background.color=0x00ffffff \
+	icon.padding_left=0 \
+	icon.padding_right=10 \
 	background.border_width=0 \
 	--subscribe battery system_woke power_source_change
