01c421b8d702653a1bbe94683f08995e8b6026d1
FormalSnake <kyaniserni@gmail.com>
2025-03-11 16:41:22 +0000

    Remove xcode-install dependency from home.nix configuration

--- home.nix
+++ home.nix
@@ -39,7 +39,6 @@
     pkgs.mousecape # Custom cursors for macOS
     pkgs.the-unarchiver # Archive extraction utility
     pkgs.google-chrome # Web browser
-    pkgs.xcode-install
   ];
 
   catppuccin.flavor = "mocha";
