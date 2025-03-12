ca203730be363af66a87b12824ffcb066e6bab65
FormalSnake <kyaniserni@gmail.com>
2025-03-12 09:42:46 +0000

    chore: Update Neovim plugin configuration for snacks.lua

--- programs/nvim/plugins/snacks.lua
+++ programs/nvim/plugins/snacks.lua
@@ -29,18 +29,18 @@ require("snacks").setup({
   dashboard = {
     enabled = true,
     sections = {
-      { section = "header" },
-      { section = "keys", gap = 1, padding = 1 },
-      { section = "startup" },
       {
         section = "terminal",
         cmd = [[
     chafa $(find "${XDG_CONFIG_HOME:-$HOME/.config}/wallnvim/" -name "*.png" | sort -R | head -1) \
     --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1
   ]],
-        random = 10,
-        pane = 2,
         height = 17,
+        padding = 1,
+      },
+      {
+        pane = 2,
+        { section = "keys", gap = 1, padding = 1 },
       },
     },
   },
@@ -73,7 +73,8 @@ Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
 Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
 Snacks.toggle.diagnostics():map("<leader>ud")
 Snacks.toggle.line_number():map("<leader>ul")
-Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map(
+  "<leader>uc")
 Snacks.toggle.treesitter():map("<leader>uT")
 Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
 Snacks.toggle.inlay_hints():map("<leader>uh")
