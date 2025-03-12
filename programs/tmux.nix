2456c3bd88bb92918d2614ff47c7bb945591c7d6
FormalSnake <kyaniserni@gmail.com>
2025-03-12 10:26:40 +0000

    feat(tmux): Add tmux session management and title customization

--- programs/tmux.nix
+++ programs/tmux.nix
@@ -12,6 +12,19 @@
       vim-tmux-navigator
     ];
     extraConfig = ''
+      if-shell '[ -z "$(tmux ls 2>/dev/null)" ]' 'new-session'
+
+      set-option -sa terminal-overrides ",xterm*:Tc"
+      set-option -g set-titles on
+      set-option -g set-titles-string "#S / #W"
+      set -ga terminal-overrides ',*:Ss=\E[%p1%d q:Se=\E[2 q'
+      set -gg allow-passthrough all
+      set -ga update-environment TERM
+      set -ga update-environment TERM_PROGRAM
+      set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
+      set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
+      set-option -g detach-on-destroy off
+      setw -g allow-passthrough on
       set-option -g status-position top
       set-option -g status-right ""
       set-option -g @continuum-restore 'on'
