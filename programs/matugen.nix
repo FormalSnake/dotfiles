59e1ec9052cb0b3cd3dafb0357e7007f89cd5f8e
FormalSnake <kyaniserni@gmail.com>
2025-03-17 15:29:19 +0000

    Add midnight-discord CSS template to Matugen config

--- programs/matugen.nix
+++ programs/matugen.nix
@@ -6,4 +6,5 @@
   home.file.".config/matugen/config.toml".source = ./matugen/config.toml;
   home.file.".config/matugen/templates/config".source = ./matugen/templates/config;
   home.file.".config/matugen/templates/neovim.template".source = ./matugen/templates/neovim.template;
+  home.file.".config/matugen/templates/midnight-discord.css".source = ./matugen/templates/midnight-discord.css;
 }
