311870972249fc3620700deb6d299830c1004bf3
FormalSnake <kyaniserni@gmail.com>
2025-05-14 12:46:46 +0100

    feat(nixos-vm): Enable ZSH and add GNOME settings

--- hosts/nixos-vm/home.nix
+++ hosts/nixos-vm/home.nix
@@ -8,19 +8,22 @@
   # Configure home directory and username specific to this host
   home.username = "kyandesutter";
   home.homeDirectory = "/home/kyandesutter";
-  
+
   # Host-specific packages for the VM
   home.packages = with pkgs; [
     firefox
     vscode
     gnome.gnome-tweaks
   ];
-  
+
   # VM-specific settings
   programs.oh-my-posh = {
     useTheme = "huvix";
   };
-  
+
+  # Explicitly enable ZSH for NixOS
+  programs.zsh.enable = true;
+
   # GNOME-specific settings
   dconf.settings = {
     "org/gnome/desktop/interface" = {
