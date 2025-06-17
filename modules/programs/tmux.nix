e28cbe4168fec5a2290227c15a9d059c3c0f0b22
FormalSnake <kyaniserni@gmail.com>
2025-06-17 13:25:50 +0100

    Update tmux configuration with Catppuccin theme and performance optimizations

--- modules/programs/tmux.nix
+++ modules/programs/tmux.nix
@@ -5,72 +5,69 @@
 }: {
   programs.tmux = {
     enable = true;
-    baseIndex = 1;
     keyMode = "vi";
-    shortcut = "b";
-    terminal = "screen-256color";
-    mouse = true;
-    clock24 = true;
+    prefix = "C-b";
     escapeTime = 0;
     historyLimit = 50000;
+    mouse = true;
+    terminal = "tmux-256color";
     
     plugins = with pkgs.tmuxPlugins; [
       {
-        plugin = resurrect;
+        plugin = tmux-sessionx;
         extraConfig = ''
-          set -g @resurrect-strategy-vim 'session'
-          set -g @resurrect-strategy-nvim 'session'
-          set -g @resurrect-capture-pane-contents 'on'
+          set -g @sessionx-bind 's'
+          set -g @sessionx-auto-accept 'off'
+          set -g @sessionx-custom-paths '~/Documents,~/Projects'
+          set -g @sessionx-filter-current 'false'
         '';
       }
-      {
-        plugin = continuum;
-        extraConfig = ''
-          set -g @continuum-restore 'on'
-          set -g @continuum-boot 'on'
-          set -g @continuum-save-interval '10'
-        '';
-      }
-      sensible
-      yank
       vim-tmux-navigator
+      catppuccin
     ];
 
     extraConfig = ''
-      # Shell and session management
-      set -g default-command /bin/zsh
-      if-shell '[ -z "$(tmux ls 2>/dev/null)" ]' 'new-session'
-
-      # Terminal capabilities
+      # Kitty image protocol support
+      set -gq allow-passthrough on
+      set -g visual-activity off
+      
+      # Terminal capabilities and true color
       set-option -sa terminal-overrides ",xterm*:Tc"
+      set -ga terminal-overrides ",*:Ss=\E[%p1%d q:Se=\E[2 q"
+      set-option -g focus-events on
+      
+      # Window title support
       set-option -g set-titles on
-      set-option -g set-titles-string "#S / #W"
-      set -ga terminal-overrides ',*:Ss=\E[%p1%d q:Se=\E[2 q'
-      set -ga update-environment TERM
-      set -ga update-environment TERM_PROGRAM
-      set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
-      set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
-      set -p allow-passthrough on
-
-      # Session management
-      set-option -g detach-on-destroy off
-
-      # Status bar configuration
-      set-option -g status-position top
-      set -g status-style "bg=#1e1e2e,fg=#eeeeee"
-      set -g status-interval 1
-      set -g status-left-length 200
-      set -g status-left "#[fg=cyan] | #[fg=green] #S#[fg=yellow]  #(basename #{pane_current_path})"
-      set -g status-right "#[fg=red] %H:%M #[fg=blue] %D"
-      set -g status-justify "absolute-centre"
-      set -g window-status-current-format "#[fg=green]#[bg=green,fg=#000000] #(bash ~/lq/iconify.bash #W) (#(basename #{pane_current_path})) #[bg=#11111b,fg=green]"
-      set -g window-status-format "#[fg=#1e1e2e]#[bg=#1e1e2e,fg=grey] #(bash ~/lq/iconify.bash #W) (#(basename #{pane_current_path})) #[bg=#1e1e2e,fg=#1e1e2e]"
-
-      # Pane and window styling
-      set -g pane-active-border-style "fg=white"
-      set -g pane-border-style "fg=white"
-      set -g message-style "bg=#1e1e2e,fg=blue"
-      set -g mode-style "bg=#b5befe,fg=#1e1e2e"
+      set-option -g set-titles-string "#T"
+      
+      # Catppuccin theme customization for system integration
+      set -g @catppuccin_flavour 'macchiato'
+      set -g @catppuccin_window_left_separator ""
+      set -g @catppuccin_window_right_separator " "
+      set -g @catppuccin_window_middle_separator " █"
+      set -g @catppuccin_window_number_position "right"
+      set -g @catppuccin_window_default_fill "number"
+      set -g @catppuccin_window_default_text "#W"
+      set -g @catppuccin_window_current_fill "number"
+      set -g @catppuccin_window_current_text "#W"
+      set -g @catppuccin_status_modules_right "date_time"
+      set -g @catppuccin_status_left_separator  " "
+      set -g @catppuccin_status_right_separator ""
+      set -g @catppuccin_status_fill "icon"
+      set -g @catppuccin_status_connect_separator "no"
+      set -g @catppuccin_date_time_text "%H:%M"
+      
+      # Performance optimizations
+      set -s escape-time 0
+      set -g display-time 1000
+      set -g status-interval 5
+      set -g automatic-rename on
+      set -g automatic-rename-format '#{b:pane_current_path}'
+      
+      # Window and pane indexing
+      set -g base-index 1
+      setw -g pane-base-index 1
+      set -g renumber-windows on
     '';
   };
 }
