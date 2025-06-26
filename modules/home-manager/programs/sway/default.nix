d777e927adfbb055f33da96cd4ca20d624512b66
FormalSnake <kyaniserni@gmail.com>
2025-06-26 21:51:20 +0100

    feat: Add rofi-wayland and emoji support to sway config

--- modules/home-manager/programs/sway/default.nix
+++ modules/home-manager/programs/sway/default.nix
@@ -30,5 +30,19 @@ in {
 
   programs.rofi = {
     enable = true;
+    package = pkgs.rofi-wayland;
+    terminal = "${pkgs.ghostty}/bin/ghostty";
+
+    extraConfig = {
+      modi = "drun";
+      show-icons = true;
+      drun-display-format = "{icon} {name}";
+      disable-history = false;
+      hide-scrollbar = true;
+      display-drun = "   Apps ";
+      sidebar-mode = true;
+    };
   };
+
+  home.packages = [pkgs.bemoji];
 }
