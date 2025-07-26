833b6cb1d5d8a2bb499b7ae05394afea47b92bcd
FormalSnake <kyaniserni@gmail.com>
2025-07-26 16:34:47 +0100

    feat: Add vscode profile extensions configuration

--- modules/home-manager/programs/vscode/default.nix
+++ modules/home-manager/programs/vscode/default.nix
@@ -6,7 +6,7 @@
 }: {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       vscodevim.vim
       yzhang.markdown-all-in-one
       astro-build.astro-vscode
