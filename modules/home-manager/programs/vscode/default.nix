11ca92a6c1ed094602036983a2b0c542132a1ae1
FormalSnake <kyaniserni@gmail.com>
2025-07-26 16:42:51 +0100

    chore: Disable vscode globally, enable mutableExtensionsDir, update extension management

--- modules/home-manager/programs/vscode/default.nix
+++ modules/home-manager/programs/vscode/default.nix
@@ -5,11 +5,17 @@
   ...
 }: {
   programs.vscode = {
-    enable = true;
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-      yzhang.markdown-all-in-one
-      astro-build.astro-vscode
-    ];
+    enable = false;
+    mutableExtensionsDir = false;
+
+    profiles.default = {
+      enableUpdateCheck = false;
+      enableExtensionUpdateCheck = false;
+      extensions = with pkgs.vscode-extensions; [
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+        astro-build.astro-vscode
+      ];
+    };
   };
 }
