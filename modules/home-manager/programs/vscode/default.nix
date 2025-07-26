85702aaa9cebefc28e8cc6bc25e2fdaf7e310530
FormalSnake <kyaniserni@gmail.com>
2025-07-26 17:21:43 +0100

    feat(nix): Add VSCode extensions and create directories

--- modules/home-manager/programs/vscode/default.nix
+++ modules/home-manager/programs/vscode/default.nix
@@ -15,7 +15,18 @@
         vscodevim.vim
         yzhang.markdown-all-in-one
         astro-build.astro-vscode
+        ms-vscode.vscode-typescript-next
+        bradlc.vscode-tailwindcss
+        esbenp.prettier-vscode
+        ms-vscode.vscode-json
+        jnoortheen.nix-ide
+        mkhl.direnv
       ];
     };
   };
+
+  # Ensure VSCode directories exist before activation
+  home.activation.createVSCodeDirs = config.lib.dag.entryBefore ["writeBoundary"] ''
+    run mkdir -p "$HOME/Library/Application Support/Code/User/globalStorage"
+  '';
 }
