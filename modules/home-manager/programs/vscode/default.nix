469e6434a24b5dd5a548e447ec261bd65337f141
FormalSnake <kyaniserni@gmail.com>
2025-07-26 16:33:51 +0100

    feat: Add VSCode extensions for Vim, Markdown, and Astro support

--- modules/home-manager/programs/vscode/default.nix
+++ modules/home-manager/programs/vscode/default.nix
@@ -6,5 +6,10 @@
 }: {
   programs.vscode = {
     enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      vscodevim.vim
+      yzhang.markdown-all-in-one
+      astro-build.astro-vscode
+    ];
   };
 }
