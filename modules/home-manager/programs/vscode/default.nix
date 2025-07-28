63654cb864f3a889a5f7d051cc64b7902da57515
FormalSnake <kyaniserni@gmail.com>
2025-07-28 18:57:21 +0100

    Add custom font settings for VSCode in home-manager configuration

--- modules/home-manager/programs/vscode/default.nix
+++ modules/home-manager/programs/vscode/default.nix
@@ -19,6 +19,12 @@
         esbenp.prettier-vscode
         jnoortheen.nix-ide
       ];
+      userSettings = {
+        "editor.fontFamily" = "GeistMono Nerd Font";
+        "terminal.integrated.fontFamily" = "GeistMono Nerd Font";
+        "debug.console.fontFamily" = "GeistMono Nerd Font";
+        "editor.codeLensFontFamily" = "GeistMono Nerd Font";
+      };
     };
   };
 }
