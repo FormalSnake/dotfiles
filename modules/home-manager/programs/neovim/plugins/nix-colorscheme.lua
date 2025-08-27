457ab05d57bbbf1cca40c8c6d8be9debef17c3e6
FormalSnake <kyaniserni@gmail.com>
2025-08-27 13:36:37 +0100

    Refactor: Simplify Neovim plugin setup and remove debug prints

--- modules/home-manager/programs/neovim/plugins/nix-colorscheme.lua
+++ modules/home-manager/programs/neovim/plugins/nix-colorscheme.lua
@@ -1,6 +1,4 @@
-print("Theme Debug: Loading nix-colorscheme.lua")
-
-local M = {}
+print("Theme Debug: Starting nix colorscheme setup")
 
 local function get_current_theme()
   local theme_file = vim.fn.expand("~/.config/nix-themes/current")
@@ -45,34 +43,24 @@ local function apply_theme()
   end)
 end
 
--- Setup function
-function M.setup()
-  print("Theme Debug: Setting up nix colorscheme system")
-  
-  -- Apply theme on startup
+-- Apply theme on startup
+apply_theme()
+
+-- Create user command to reload theme manually
+vim.api.nvim_create_user_command("ReloadTheme", function()
+  print("Theme Debug: Manual reload triggered")
   apply_theme()
-  
-  -- Create user command to reload theme manually
-  vim.api.nvim_create_user_command("ReloadTheme", function()
-    print("Theme Debug: Manual reload triggered")
+  vim.notify("Theme reloaded!", vim.log.levels.INFO)
+end, {})
+
+-- Watch for theme changes and reload
+vim.api.nvim_create_autocmd("Signal", {
+  pattern = "SIGUSR1", 
+  callback = function()
+    print("Theme Debug: Received SIGUSR1 signal")
     apply_theme()
-    vim.notify("Theme reloaded!", vim.log.levels.INFO)
-  end, {})
-  
-  -- Watch for theme changes and reload
-  vim.api.nvim_create_autocmd("Signal", {
-    pattern = "SIGUSR1", 
-    callback = function()
-      print("Theme Debug: Received SIGUSR1 signal")
-      apply_theme()
-      vim.notify("Theme reloaded via signal!", vim.log.levels.INFO)
-    end,
-  })
-  
-  print("Theme Debug: Setup complete")
-end
+    vim.notify("Theme reloaded via signal!", vim.log.levels.INFO)
+  end,
+})
 
--- Also expose the apply function
-M.apply_theme = apply_theme
-
-return M
+print("Theme Debug: Setup complete")
