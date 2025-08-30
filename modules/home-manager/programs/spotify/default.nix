cc5e0897bea0e938b36cb2d44eb17927412f1e67
FormalSnake <kyaniserni@gmail.com>
2025-08-30 15:12:53 +0100

    Disable Spotify Spicetify integration  Y4:0

--- modules/home-manager/programs/spotify/default.nix
+++ modules/home-manager/programs/spotify/default.nix
@@ -7,7 +7,7 @@
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
 in {
   programs.spicetify = {
-    enable = true;
+    enable = false;
     theme = spicePkgs.themes.catppuccin;
     colorScheme = "mocha";
     # enabledExtensions = with spicePkgs.extensions; [
