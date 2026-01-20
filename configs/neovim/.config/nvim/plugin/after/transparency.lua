f2620ced6fc0e76470ee838d688204d4d6b045b2
FormalSnake <kyaniserni@gmail.com>
2026-01-20 07:37:21 +0000

    images

--- configs/neovim/.config/nvim/plugin/after/transparency.lua
+++ configs/neovim/.config/nvim/plugin/after/transparency.lua
@@ -1,45 +1,61 @@
--- transparent background
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
-vim.api.nvim_set_hl(0, "Terminal", { bg = "none" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
-vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg = "none" })
+-- Transparent background - applies after every colorscheme change
+-- This ensures transparency persists when auto-dark-mode switches themes
 
--- transparent background for neotree
-vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
-vim.api.nvim_set_hl(0, "NeoTreeVertSplit", { bg = "none" })
-vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { bg = "none" })
-vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "none" })
+local function apply_transparency()
+  -- Core UI elements
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Terminal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+  vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
+  vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+  vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "none" })
 
--- transparent background for nvim-tree
-vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NvimTreeVertSplit", { bg = "none" })
-vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { bg = "none" })
+  -- Telescope
+  vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+  vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
+  vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg = "none" })
 
--- transparent notify background
-vim.api.nvim_set_hl(0, "NotifyINFOBody", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyERRORBody", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyWARNBody", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyTRACEBody", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyDEBUGBody", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyINFOTitle", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyERRORTitle", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyWARNTitle", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyTRACETitle", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyDEBUGTitle", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyINFOBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyERRORBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyWARNBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyTRACEBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "NotifyDEBUGBorder", { bg = "none" })
+  -- NeoTree
+  vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NeoTreeVertSplit", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "none" })
+
+  -- NvimTree
+  vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NvimTreeVertSplit", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { bg = "none" })
+
+  -- Notify
+  vim.api.nvim_set_hl(0, "NotifyINFOBody", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyERRORBody", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyWARNBody", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyTRACEBody", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyDEBUGBody", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyINFOTitle", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyERRORTitle", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyWARNTitle", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyTRACETitle", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyDEBUGTitle", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyINFOBorder", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyERRORBorder", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyWARNBorder", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyTRACEBorder", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NotifyDEBUGBorder", { bg = "none" })
+end
+
+-- Apply transparency after every colorscheme change
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = apply_transparency,
+})
+
+-- Apply immediately for current session
+apply_transparency()
