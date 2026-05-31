9d012165ed0cb23bd7da227a869e9a886a3e0765
FormalSnake <kyaniserni@gmail.com>
2026-05-31 14:05:13 +0100

    feat(config): add pi-web-access and enforce git-lfs ini

--- users/kyandesutter/mixins/git.nix
+++ users/kyandesutter/mixins/git.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 {
   programs.git = {
     enable = true;
@@ -12,6 +13,16 @@
       init.defaultBranch = "main";
       http.postBuffer = 157286400;
       pull.rebase = false;
+    };
+
+    # Override home-manager's lfs.enable which writes absolute nix-store
+    # paths into the filter config. GitHub Desktop expects the canonical
+    # `git-lfs ...` form (it warns otherwise).
+    iniContent.filter.lfs = {
+      clean = lib.mkForce "git-lfs clean -- %f";
+      smudge = lib.mkForce "git-lfs smudge -- %f";
+      process = lib.mkForce "git-lfs filter-process";
+      required = lib.mkForce true;
     };
   };
 }
