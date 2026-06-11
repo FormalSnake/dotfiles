a6061499e9cf16949a33caf3e9f6e4b513410c0f
FormalSnake <kyaniserni@gmail.com>
2026-06-11 13:01:27 +0100

    fix(home-manager): disable manual manpages to avoid warning

--- modules/darwin/mixins/home-manager.nix
+++ modules/darwin/mixins/home-manager.nix
@@ -7,5 +7,14 @@
     useUserPackages = true;
     extraSpecialArgs = { inherit inputs; };
     backupFileExtension = "before-hm";
+
+    sharedModules = [
+      {
+        # Avoid building Home Manager's option documentation JSON during
+        # activation. With this config, that derivation warns about a store path
+        # in the generated options data losing its Nix string context.
+        manual.manpages.enable = false;
+      }
+    ];
   };
 }
