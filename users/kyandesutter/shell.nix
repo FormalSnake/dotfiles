7507d245a0c134edace896480a3552c2e6c55f76
FormalSnake <kyaniserni@gmail.com>
2026-07-02 12:47:25 +0100

    portless env var

--- users/kyandesutter/shell.nix
+++ users/kyandesutter/shell.nix
@@ -6,6 +6,10 @@
     PAGER = "less";
     LESS = "-FRX";
 
+    # Share portless dev servers on my tailnet by default (equivalent to
+    # `portless --tailscale`). Honoured by yarn, bun, npm and direct portless.
+    PORTLESS_TAILSCALE = "1";
+
     JAVA_HOME = "${pkgs.zulu21.home}";
   } // lib.optionalAttrs pkgs.stdenv.isDarwin {
     # macOS-only: the SDK lives under the macOS Library path (wrong on Linux).
